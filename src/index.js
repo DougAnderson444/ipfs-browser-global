d3b1e699326b714b88e7ea8b47009047e6b12118
Doug A <douganderson444@gmail.com>
2022-02-15 11:00:50 -0400

    add config and dag usage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import { use, CID as cid } from './IPFS.js'
+import { use, CID as cid } from "./IPFS.js"
 
 // (async () => {
 //   await use()
 // })()
 export const CID = cid
 
-export default async () => {
-  await use()
+export default async (config = {}) => {
+    await use(config)
 
-  // Now you can use ipfs globally to do things like
+    // Now you can use ipfs globally to do things like
 
-  // const ipfsNode = ipfs // bind the prop to the global
-  // const identity = await ipfsNode.id()
-  // const nodeId = identity.id
-  // console.info('nodeId', nodeId)
+    // const ipfsNode = ipfs // bind the prop to the global
+    // const identity = await ipfsNode.id()
+    // const nodeId = identity.id
+    // console.info("nodeId", nodeId)
+    // const rootCID = await ipfs.dag.put(saveObject, { pin: true })
+
+    // const res = await ipfs.dag.get(rootCID)
+    // console.log(res.value)
 }
